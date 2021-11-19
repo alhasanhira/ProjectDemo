b5501d0583340da61ca774f3600ac0790ac821dd
alhasanhira <al-hasan.hira@rittec.de>
2021-11-19 11:04:04 +0100

    Hira's commit again

--- src/com/ProjectDemo/Client.java
+++ src/com/ProjectDemo/Client.java
@@ -1,5 +1,8 @@
 package com.ProjectDemo;
 
+import com.ProjectDemo.ConstantTexts;
+import com.ProjectDemo.Login;
+
 import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
@@ -23,53 +26,37 @@ public class Client {
     }
 
     public static void main(String[] args) throws IOException {
-<<<<<<< HEAD
-        Socket clientSocket = new Socket(host,port);
+        Socket clientSocket = new Socket(host, port);
         Client client = new Client(clientSocket);
         Scanner userInput = new Scanner(System.in);
         while (true){
             System.out.println("1.Register\n2.Login");
             String ans = userInput.nextLine();
             switch (ans) {
-                case "0" : break;
+                case "0":
+                    break;
                 case "1":
                     System.out.println("Enter Your Username:");
                     String username = userInput.nextLine();
                     System.out.println("Enter a password:");
                     String password = userInput.nextLine();
-                    String data =ConstantTexts.register+":"+username+":"+password;
+                    String data = ConstantTexts.register + ":" + username + ":" + password;
                     client.sendMessage(data);
                     break;
-                case "2" :
+                case "2":
                     Login.loginUser(userInput);
                     break;
             }
         }
-=======
-        Scanner scanner = new Scanner(System.in);
-        //put the username on the Active user map.
 
-        System.out.println("Enter your username to join the chat: ");
-        String username = scanner.nextLine();
-        Socket socket = new Socket(host,port);
-        Client client = new Client(socket,username);
-
-        System.out.println("You joined the chat.");
-        client.listenMessage();
-        client.sendMessage();
-
-
->>>>>>> 443800ded04cf94eb519b8801fb1e2530b742009
     }
-    //just display things here..?
 
     public void sendMessage(String sendTo){
         try{
-            while (clientSocket.isConnected()){
-                bufferedWriter.write(sendTo);
-                bufferedWriter.newLine();
-                bufferedWriter.flush();
-            }
+            bufferedWriter.write(sendTo);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+
         }catch (IOException e){
             closeEverything(clientSocket, bufferedWriter, bufferedReader);
         }
